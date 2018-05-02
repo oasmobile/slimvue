93f67bf717303ecd256c63403e08b1e96f61916a
Minhao Zhang <minhao.zhang@qq.com>
2018-05-02 15:35:59 +0800

    * fix IDE warnings

--- src/SlimVueUpgradeCommand.php
+++ src/SlimVueUpgradeCommand.php
@@ -9,12 +9,9 @@
 namespace Oasis\SlimVue;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
 
 class SlimVueUpgradeCommand extends Command
