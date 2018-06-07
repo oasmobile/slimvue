a6efd71fad21c88ea4db2085de42391e0c8f99c3
Minhao Zhang <minhao.zhang@qq.com>
2018-06-07 18:33:55 +0800

    * fix: post-install script now supports scoped package

--- slimvue-template/build/post-install.js
+++ slimvue-template/build/post-install.js
@@ -2,7 +2,8 @@ require('./check-versions')();
 const fs = require('fs-extra');
 const resolveConfig = require('./resolve.conf');
 
-let inHomeDir = !(fs.pathExistsSync(resolveConfig.resolve('../../node_modules')));
+let parentProjectDir = resolveConfig.resolve(resolveConfig.projectName.startsWith("@") ? '../../..' : '../..');
+let inHomeDir = !(fs.pathExistsSync(parentProjectDir + "/node_modules"));
 let resolves = {};
 
 if (inHomeDir) {
@@ -15,19 +16,19 @@ else {
     let transformToRequireFilePath = resolveConfig.resolve('config/transform-settings.json');
     if (fs.existsSync(transformToRequireFilePath)) {
         console.log("Will generate transform to require file ...");
-        let targetTransformToRequireFilePath = resolveConfig.resolve('../../build/custom-transform-to-require-settings/' + resolveConfig.projectName + '.json');
+        let targetTransformToRequireFilePath = parentProjectDir + '/build/custom-transform-to-require-settings/' + resolveConfig.projectName + '.json';
         fs.copySync(transformToRequireFilePath, targetTransformToRequireFilePath);
         console.log("Custom transform to require file copied");
     }
     
     console.log("Will generate resolve dependency file ...");
-    depFilePath = resolveConfig.resolve('../../build/resolve-deps/' + resolveConfig.projectName + '.json');
+    depFilePath = parentProjectDir + '/build/resolve-deps/' + resolveConfig.projectName + '.json';
     resolves[resolveConfig.projectName + '$'] = "node_modules/" + resolveConfig.projectName + "/" + resolveConfig.projectName + "/publish.js";
     resolves[resolveConfig.projectName + "/" + resolveConfig.projectName] = "node_modules/" + resolveConfig.projectName + "/" + resolveConfig.projectName;
     resolves[resolveConfig.projectName] = "node_modules/" + resolveConfig.projectName + "/" + resolveConfig.projectName;
     
     console.log("Will generate copy-file-setting file ...");
-    settingFilePath = resolveConfig.resolve('../../build/copy-file-settings/' + resolveConfig.projectName + '.json');
+    settingFilePath = parentProjectDir + '/build/copy-file-settings/' + resolveConfig.projectName + '.json';
     fs.outputJsonSync(
         settingFilePath,
         {
