c9e12d646897044e10cb956db8b08f9ede820300
zhangxin <915466@gmail.com>
2020-05-09 17:50:36 +0800

    + devServer

--- slimvue-template/vue.config.js
+++ slimvue-template/vue.config.js
@@ -2,6 +2,7 @@ let path = require("path"),
     pages = require("./build/entries.js"),
     slimvueConfig = require("./build/config.js"),
     webpackConfig = require("./build/webpack-chain/index.js"),
+    devServer = require("./build/devServer.js"),
     transformAssetUrls = require("./build/transformAssetUrls.js"),
     copyToDir = path.join(__dirname, slimvueConfig.outputDir, "static");
 
@@ -10,6 +11,7 @@ module.exports = {
     outputDir: slimvueConfig.outputDir,
     assetsDir: slimvueConfig.assetsDir,
     pages,
+    devServer,
     chainWebpack(config) {
         config.merge(webpackConfig);
         config.module
