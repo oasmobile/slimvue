c46755c39d1f7460810e22481acb6ebb5c0db73a
zhangxin <915466@gmail.com>
2020-05-09 17:10:22 +0800

    init

--- slimvue-template/vue.config.js
+++ slimvue-template/vue.config.js
@@ -1,7 +1,9 @@
-let pages = require("./build/entries.js"),
+let path = require("path"),
+    pages = require("./build/entries.js"),
     slimvueConfig = require("./build/config.js"),
     webpackConfig = require("./build/webpack-chain/index.js"),
-    transformAssetUrls = require("./build/transformAssetUrls.js");
+    transformAssetUrls = require("./build/transformAssetUrls.js"),
+    copyToDir = path.join(__dirname, slimvueConfig.outputDir, "static");
 
 module.exports = {
     publicPath: slimvueConfig.publicPath,
@@ -17,5 +19,10 @@ module.exports = {
                 options.transformAssetUrls = transformAssetUrls;
                 return options;
             });
+        config.plugin("copy").tap(args => {
+            let opts = [...args[0]];
+            opts.forEach(opt => (opt.to = copyToDir));
+            return [opts];
+        });
     }
 };
