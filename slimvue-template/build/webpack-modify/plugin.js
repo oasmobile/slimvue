85c9254173f8392f46e74a769930b73078d8c157
zhangxin <915466@gmail.com>
2020-05-21 15:58:44 +0800

    fix copy plugin

--- slimvue-template/build/webpack-modify/plugin.js
+++ slimvue-template/build/webpack-modify/plugin.js
@@ -1,13 +1,22 @@
 const path = require("path"),
     slimvueConfig = require("../config.js"),
-    copyToDir = path.join(__dirname, "../..", slimvueConfig.outputDir, "static");
+    copyToDir = path.join(
+        __dirname,
+        "../..",
+        slimvueConfig.outputDir,
+        "static"
+    );
 
 const modify = config => {
-    config.plugin("copy").tap(args => {
-        let opts = [...args[0]];
-        opts.forEach(opt => (opt.to = copyToDir));
-        return [opts];
-    });
+    config.plugins.has("copy") &&
+        config.plugin("copy").tap(args => {
+            if (args.length) {
+                let opts = [...args[0]];
+                opts.forEach(opt => (opt.to = copyToDir));
+                return [opts];
+            }
+            return args;
+        });
 };
 
 module.exports = modify;
